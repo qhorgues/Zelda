d97828f25fbc2f539b9c749993be01687ef6d5bb
Quentin Horgues <quentin.horgues@outlook.fr>
2023-04-28 11:50:05 +0200

    Creation d'une classe en charge des coordonnées

--- src/Coord.hpp
+++ src/Coord.hpp
@@ -1,10 +1,48 @@
 #ifndef COORD_HPP
 #define COORD_HPP
 
-struct Coord
+#include <utility>
+#include <concepts>
+
+template <typename T = int> requires std::is_arithmetic<T>::value
+class Coord final
 {
-    int x;
-    int y;
+public:
+    constexpr Coord() = default;
+    constexpr Coord(T x, T y) noexcept
+     : m_coord(x, y)
+    {
+    }
+    constexpr ~Coord() = default;
+
+    constexpr Coord(Coord const&) = default;
+    constexpr Coord& operator=(Coord const&) = default;
+
+    constexpr Coord(Coord &&) = default;
+    constexpr Coord& operator=(Coord &&) = default;
+
+    constexpr T get_x(void) const noexcept
+    {
+        return m_coord.first;
+    }
+
+    constexpr T get_y(void) const noexcept
+    {
+        return  m_coord.second;
+    }
+
+    constexpr void set_x(T x) noexcept
+    {
+        m_coord.first = x;
+    }
+
+    constexpr void set_y(T y) noexcept
+    {
+        m_coord.second = y;
+    }
+
+private:
+    std::pair<int, int> m_coord;
 };
 
 #endif
