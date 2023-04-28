648e96f1539e47b19c83115dde699126b6dd2c38
Quentin Horgues <quentin.horgues@outlook.fr>
2023-04-28 11:52:53 +0200

    teste de l'attaque

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <exception>
-#include "Engine/Character_with_life/Character_with_life.hpp"
-#include "Engine/Movable_Character/Movable_Character.hpp"
-#include "Engine/Non_Movable_Character/Non_Movable_Character.hpp"
+#include "Engine/Engine.hpp"
 
 namespace 
 {
@@ -10,26 +8,33 @@ namespace
     constexpr int HEIGHT_LINK_SPRITE {27};
 };
 
-class A : public Engine::Character_with_life, public Engine::Movable_Character
+class A : public Engine::Character_with_life, public Engine::Movable_Character, public Engine::Character_with_attack
 {
 public:
-    A(int x, int y, double velocity, int max_life) : Character_with_life(max_life), Movable_Character(x, y, velocity)
+    A(int x, int y, double velocity, double max_life, double hitpoint) noexcept
+     : Character_with_life(max_life), Movable_Character(x, y, velocity), Character_with_attack(hitpoint)
     {}
 
 };
 
 int main()
 {
-    A a {12, 15, 0.5, 50};
+    A a {12, 15, 0.5, 50., 6.};
+    Engine::Character_with_attack b {9};
 
     std::cout << a.get_life() << std::endl;
-    a.add_more_max_life(20);
+    a.add_more_max_life(20.);
     a.regeneration(40);
     std::cout << a.get_life() << std::endl;
 
+    b.attack(a);
+
+    std::cout << a.get_life() << std::endl;
+
+
     a.move(Engine::Movable_Character::Direction::EST, 2.);
 
-    auto[x, y] = a.get_position();
+    auto[x, y] = a.get_position_x_y();
 
     std::cout << x << " / " << y << std::endl;
 }
